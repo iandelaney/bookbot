1bbcc8ac02b179a203a8fd50311bd90a25bc3fe6
iandelaney <delaney.ian@gmail.com>
2025-12-22 23:55:59 +0000

    got to the end

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-from stats import word_count, char_count
+import sys
 
+from stats import word_count, char_count, sort_char_count   
 
 def get_book_text(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
 def main():
-    book_path = 'books/frankenstein.txt'
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     return book_text
     #print(book_text)
-
-
     
-print(word_count(main()))
-print(char_count(main()))
-
-
+# print(word_count(main()))
+# print(char_count(main()))
 # main()
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+#print pretty report
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {sys.argv[1]}...")
+print("----------- Word Count ----------")
+print(f"Found {word_count(main())} total words")
+print("--------- Character Count -------")
+
+my_dict = (sort_char_count(char_count(main())))
+
+for k, v in my_dict.items():
+	print (k+':', v)
+     
+print("=================================")
--- stats.py
+++ stats.py
@@ -1,12 +1,18 @@
 def word_count(text):
     words = len(text.split())
-    print(f"Found {words} total words")
+    #print(f"Found {words} total words")
+    return words
 
 def char_count(text):
     d = {}
     for x in text.lower():
-        if x in d:
-            d[x] = d[x] + 1
-        else:
-            d[x] = 1
+        if x.isalpha():
+            if x in d:
+                d[x] = d[x] + 1
+            else:
+                d[x] = 1
     return d
+
+def sort_char_count(char_dict):
+    return dict(sorted(char_dict.items(), key=lambda item: item[1], reverse=True))
+
